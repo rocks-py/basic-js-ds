8679474b4d1e4b3d6538a97f99985eba62112891
Artem <75018018+rocks-py@users.noreply.github.com>
2022-05-02 10:33:35 -0700

    feat: solve queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,23 +14,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  list = null;
+  first = null;
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.first;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    if (this.list === null) {
+      this.list = new ListNode();
+      this.list.value = value;
+      this.first = this.list;
+      return;
+    }
+
+    this.list.next = new ListNode();
+    this.list = this.list.next;
+    this.list.value = value;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.first && this.first.value) {
+      let val = this.first.value;
+      this.first = this.first.next;
+      return val;
+    }
   }
 }
 
 module.exports = {
   Queue
 };
+
+
+// const queue = new Queue();
+
+// queue.enqueue(1); // добавляет элемент в очередь
+// queue.enqueue(3); // добавляет элемент в очередь
+// queue.dequeue(); // возвращает элемент из начала очереди и удаляет его, возвращает 1
+// queue.getUnderlyingList() // возвращает { value: 3, next: null }
